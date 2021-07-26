2bfd803038f66e5c02db9a41fe7aee6699d68785
Rodrigo <rlsnetvendas@gmail.com>
2021-07-26 16:43:28 -0300

    Deploy

---
+++ pergunta.js
@@ -0,0 +1,53 @@
+
+let perguntaJson = [
+    {
+        id:0, 
+        questao:'De quem é a famosa frase “Penso, logo existo”?', 
+        option1:'a) Platão', 
+        option2:'b) Galileu Galilei',
+        option3:'c) Descartes',
+        option4:'d) Sócrates',
+        option5:'e) Francis Bacon',
+        respCerta: 'option3'
+     },
+    {
+        id:1, 
+        questao:'De onde é a invenção do chuveiro elétrico?', 
+        option1:'a) França', 
+        option2:'b) Estados Unidos',
+        option3:'c) Inglaterra',
+        option4:'d) Alemanha',
+        option5:'e) Brasil',
+        respCerta: 'option5'
+     },
+    {
+        id:2, 
+        questao:'Quais o menor e o maior país do mundo?', 
+        option1:'a) São Marino e Índia', 
+        option2:'b) Mônaco e Canadá',
+        option3:'c) Malta e Estados Unidos',
+        option4:'d) Vaticano e Rússia',
+        option5:'e) Nauru e China',
+        respCerta: 'option4'
+     },
+    {
+        id:3, 
+        questao:'Qual o livro mais vendido no mundo a seguir à Bíblia?', 
+        option1:'a) O Senhor dos Anéis', 
+        option2:'b) Dom Quixote',
+        option3:'c) O Pequeno Príncipe',
+        option4:'d) Ela, a Feiticeira',
+        option5:'e) Um Conto de Duas Cidades',
+        respCerta: 'option2'
+     },
+    {
+        id:4, 
+        questao:'O que a palavra legend significa em português?', 
+        option1:'a) Legenda', 
+        option2:'b) Conto',
+        option3:'c) História',
+        option4:'d) Lenda',
+        option5:'e) Legendário',
+        respCerta: 'option4'
+     },
+];
